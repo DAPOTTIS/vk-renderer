e04160b16b73e71ba13480981c05bd22e717e5b7
ms <mohamedsaafan04@hotmail.com>
2025-02-26 20:29:53 +0200

    Initial commit

---
+++ lve_window.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+namespace lve {
+    class LveWindow {
+    public:
+        LveWindow(int w, int h, std::string name);
+
+        ~LveWindow();
+
+        LveWindow(const LveWindow &) = delete;
+
+        LveWindow &operator=(const LveWindow &) = delete;
+        LveWindow() = default;
+    
+        bool shouldClose() { return glfwWindowShouldClose(window); }
+        VkExtent2D getExtent() { return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)}; }
+        bool wasWindowResized() {return frameBufferResized;}
+        void resetWindowResized() { frameBufferResized = 0; }
+        
+        void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
+
+    private:
+        static void frameBufferResizeCallback(GLFWwindow *window, int width, int height);
+        void initWindow();
+
+        int width;
+        int height;
+        bool frameBufferResized = false;
+
+        std::string windowName;
+        GLFWwindow *window;
+    };
+} // namespace lve
